dcf97f5b379b0bc4a2c5f6ae84f0e72ab1460698
bbahd30 <b_bonal@cs.iitr.ac.in>
2024-04-10 18:29:46 +0530

    Add Order feature

---
+++ frontend/src/Components/Suppliers/SupplierRow.jsx
@@ -0,0 +1,255 @@
+import React, { useEffect, useState } from "react";
+import {
+	Collapse,
+	IconButton,
+	Typography,
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TextField,
+	Button,
+	TableRow,
+	Grow,
+	Box,
+} from "@mui/material";
+import { useDispatch } from "react-redux";
+
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import AddIcon from "@mui/icons-material/Add";
+import { setSupplierID, setDialogOpen } from "../../Slices/supplierSlice";
+import { addQuantity, removeItem } from "../../Slices/cartSlice";
+
+// import SupplierDialog from "./SupplierDialog";
+
+const AddQuantityBtn = ({ medicine, supplierId }) => {
+	const [checked, setChecked] = useState(false);
+	const [quantity, setQuantity] = useState(0);
+	const dispatch = useDispatch();
+	const addToCart = (medicineId, name, supplierId, quantity) => {
+		if (quantity && quantity > 0) {
+			dispatch(addQuantity({ medicineId, supplierId, quantity, name }));
+		}
+	};
+	const handleChange = () => {
+		setChecked((prev) => !prev);
+	};
+	return (
+		<>
+			{!checked && <Button onClick={handleChange}>Add Quantity</Button>}
+			{checked && (
+				<Grow
+					in={checked}
+					style={{ transformOrigin: "0 0 0" }}
+					{...(checked ? { timeout: 1000 } : {})}
+				>
+					<div
+						style={{
+							display: "flex",
+							alignItems: "center",
+							justifyContent: "center",
+						}}
+					>
+						<div
+							style={{
+								display: "flex",
+								justifyContent: "stretch",
+							}}
+						>
+							<TextField
+								type="number"
+								inputProps={{ min: 1 }}
+								value={quantity}
+								onChange={(e) =>
+									setQuantity(parseInt(e.target.value, 10))
+								}
+							/>
+							<Button
+								variant="outlined"
+								onClick={() =>
+									addToCart(
+										medicine.ID,
+										medicine.name,
+										supplierId,
+										quantity
+									)
+								}
+							>
+								<AddIcon />
+							</Button>
+						</div>
+					</div>
+				</Grow>
+			)}
+		</>
+	);
+};
+
+const SupplierRow = (props) => {
+	const dispatch = useDispatch();
+	const { row } = props;
+	const [quantity, setQuantity] = useState([]);
+
+	const handleQuantityChange = (medicineId, value) => {
+		const itemIndex = quantity.findIndex(
+			(item) => item.medicineId === medicineId
+		);
+
+		if (itemIndex === -1) {
+			setQuantity([...quantity, { medicineId, quantity: value }]);
+		} else {
+			const updatedQuantity = [...quantity];
+			updatedQuantity[itemIndex].quantity = value;
+			setQuantity(updatedQuantity);
+		}
+	};
+	useEffect(() => {
+		console.log(quantity);
+	}, [quantity]);
+
+	const [open, setOpen] = useState(false);
+	const medicines = row.medicines;
+
+	return (
+		<React.Fragment>
+			<TableRow
+				sx={{
+					"& > *": { borderBottom: "unset" },
+					backgroundColor: "#f5f5f5",
+				}}
+				onClick={(event) => {
+					if (event.target.cellIndex !== 0) {
+						dispatch(setSupplierID(row.ID));
+						dispatch(setDialogOpen());
+					}
+				}}
+			>
+				<TableCell sx={{ fontFamily: "Poppins" }}>
+					<IconButton
+						aria-label="expand row"
+						size="small"
+						onClick={(e) => {
+							e.stopPropagation();
+							setOpen(!open);
+						}}
+					>
+						{open ? (
+							<KeyboardArrowUpIcon />
+						) : (
+							<KeyboardArrowDownIcon />
+						)}
+					</IconButton>
+				</TableCell>
+				<TableCell
+					sx={{ fontFamily: "Poppins" }}
+					component="th"
+					scope="row"
+				>
+					{row.name}
+				</TableCell>
+				<TableCell sx={{ fontFamily: "Poppins" }}>
+					{row.email}
+				</TableCell>
+				<TableCell sx={{ fontFamily: "Poppins" }}>
+					{row.phone}
+				</TableCell>
+				<TableCell sx={{ fontFamily: "Poppins" }}>
+					{row.joiningDate}
+				</TableCell>
+			</TableRow>
+			<TableRow>
+				<TableCell
+					sx={{ fontFamily: "Poppins" }}
+					style={{ paddingBottom: 0, paddingTop: 0 }}
+					colSpan={7}
+				>
+					<Collapse in={open} timeout="auto" unmountOnExit>
+						<Box sx={{ margin: 1, px: 15 }}>
+							<Typography
+								fontFamily={"Poppins"}
+								variant="body1"
+								fontWeight={500}
+								gutterBottom
+								component="div"
+							>
+								Medicines They Provide
+							</Typography>
+							<Table size="small" aria-label="details">
+								<TableHead>
+									<TableRow>
+										<TableCell
+											sx={{ fontFamily: "Poppins" }}
+										>
+											Name
+										</TableCell>
+										<TableCell
+											sx={{ fontFamily: "Poppins" }}
+										>
+											Type
+										</TableCell>
+										<TableCell
+											sx={{ fontFamily: "Poppins" }}
+										>
+											Price
+										</TableCell>
+										<TableCell
+											sx={{ fontFamily: "Poppins" }}
+										>
+											Expiry Date
+										</TableCell>
+										<TableCell />
+									</TableRow>
+								</TableHead>
+								<TableBody>
+									{medicines.map((medicine) => (
+										<React.Fragment key={medicine.ID}>
+											<TableRow>
+												<TableCell
+													sx={{
+														fontFamily: "Poppins",
+													}}
+												>
+													{medicine.name}
+												</TableCell>
+												<TableCell
+													sx={{
+														fontFamily: "Poppins",
+													}}
+												>
+													{medicine.type}
+												</TableCell>
+												<TableCell
+													sx={{
+														fontFamily: "Poppins",
+													}}
+												>
+													{medicine.price}
+												</TableCell>
+												<TableCell
+													sx={{
+														fontFamily: "Poppins",
+													}}
+												>
+													{medicine.expiryDate}
+												</TableCell>
+												<TableCell align="center">
+													<AddQuantityBtn
+														medicine={medicine}
+														supplierId={row.ID}
+													/>
+												</TableCell>
+											</TableRow>
+										</React.Fragment>
+									))}
+								</TableBody>
+							</Table>
+						</Box>
+					</Collapse>
+				</TableCell>
+			</TableRow>
+			{/* <SupplierDialog /> */}
+		</React.Fragment>
+	);
+};
+export default SupplierRow;
--- frontend/src/Slices/cartSlice.js
+++ frontend/src/Slices/cartSlice.js
@@ -2,27 +2,87 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
 	items: [],
+	snackbarOpen: false,
+	snackbarMsg: "",
 };
 
 export const cartSlice = createSlice({
 	name: "cart",
 	initialState,
 	reducers: {
-		addToCart: (state, action) => {
-			state.items.push(action.payload);
+		addOneToCart: (state, action) => {
+			const { medicineId, supplierId } = action.payload;
+			const existingItem = state.items.find(
+				(item) =>
+					item.medicineId === medicineId &&
+					item.supplierId === supplierId
+			);
+			if (existingItem) {
+				existingItem.quantity += 1;
+			} else {
+				state.items.push({ medicineId, supplierId, quantity: 1 });
+			}
 		},
-		removeFromCart: (state, action) => {
+		removeOneFromCart: (state, action) => {
+			const { medicineId, supplierId } = action.payload;
+			const existingItem = state.items.find(
+				(item) =>
+					item.medicineId === medicineId &&
+					item.supplierId === supplierId
+			);
+			if (existingItem) {
+				if (existingItem.quantity === 1) {
+					state.items = state.items.filter(
+						(item) =>
+							item.medicineId !== medicineId ||
+							item.supplierId !== supplierId
+					);
+				} else {
+					existingItem.quantity -= 1;
+				}
+			}
+		},
+		addQuantity: (state, action) => {
+			const { medicineId, supplierId, quantity, name } = action.payload;
+			const existingItem = state.items.find(
+				(item) =>
+					item.medicineId === medicineId &&
+					item.supplierId === supplierId
+			);
+
+			if (existingItem) {
+				existingItem.quantity = quantity;
+			} else {
+				state.items.push({ medicineId, supplierId, quantity });
+			}
+			state.snackbarMsg = `${name} added to cart`;
+			state.snackbarOpen = true;
+		},
+		removeItem: (state, action) => {
+			const { medicineId, supplierId } = action.payload;
 			state.items = state.items.filter(
-				(item) => item.id !== action.payload.id
+				(item) =>
+					item.medicineId !== medicineId ||
+					item.supplierId !== supplierId
 			);
 		},
 		clearCart: (state) => {
 			state.items = [];
 		},
+		closeSnackbar: (state) => {
+			state.snackbarOpen = false;
+		},
 	},
 });
 
-export const { addToCart, removeFromCart, clearCart } = cartSlice.actions;
+export const {
+	addOneToCart,
+	removeOneFromCart,
+	addQuantity,
+	removeItem,
+	clearCart,
+	closeSnackbar,
+} = cartSlice.actions;
 
 export const selectCartItems = (state) => state.cart.items;
 export const selectMedicines = (state, selectedSupplierId) =>
