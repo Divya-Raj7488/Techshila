834806a2f8d08c61f5364e13d8ff42483585d777
diEve26 <dshah7707@gmail.com>
2024-04-11 14:19:27 +0530

    Navbar on all

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	useLocation,
-} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, useLocation} from "react-router-dom";
 import SuppliersPage from "./Pages/SuppliersPage";
 import Sidebar from './Pages/Sidebar'; 
 import AddSupplier from './Pages/AddSupplierPage'
@@ -20,25 +15,14 @@ function App() {
 		
 		<>
 			<Router>
+				<Bar />
 				<Routes>
-					<Route
-						exact
-						path="/suppliers"
-						element={<SuppliersPage />}
-					/>
+					<Route exact path="/suppliers" element={<SuppliersPage />} />
 					<Route path="/add-supplier" element={<AddSupplier />} />
-          <Route path="/add-store-manager" element={<AddStoreManager />} />
-          <Route path="/*" element={<Sidebar/>} /> {/* Default route */}
-					<Route
-						exact
-						path="/bar"
-						element={<Bar />}
-					/>
-					<Route
-						exact
-						path="/login"
-						element={<Login />}
-					/>
+                    <Route path="/add-store-manager" element={<AddStoreManager />} />
+                    <Route path="/*" element={<Sidebar/>} /> {/* Default route */}
+					<Route exact path="/bar" element={<Bar />} />
+					<Route exact path="/login" element={<Login />} />
 				</Routes>
 			</Router>
 		</>
