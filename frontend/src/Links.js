d3202680bd914ddfb069a1e27380d7fedaba521a
bbahd30 <b_bonal@cs.iitr.ac.in>
2024-04-11 15:55:07 +0530

    Integrate login

--- frontend/src/Links.js
+++ frontend/src/Links.js
@@ -1,4 +1,7 @@
-export const localhost = "http://127.0.0.1:8000/";
+export const localhost = "http://127.0.0.1:3000/";
+export const userApi = `${localhost}user/`;
+export const loginApi = `${userApi}login/`;
+export const signupApi = `${userApi}signup/`;
 export const supplierApi = `${localhost}supplier/`;
 export const stockApi = `${localhost}stock/`;
 export const inventoryApi = `${localhost}inventory/`;
